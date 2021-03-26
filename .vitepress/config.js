a1e3827de402a0657188bcaef904eb9354122165
takurinton <taryochanmannn@icloud.com>
2021-03-26 12:31:58 +0900

    fix: meta

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -3,19 +3,27 @@ module.exports = {
     title: 'たくりんとんのメモ帳',
     description: 'たくりんとん',
     head: [
-        [
-            'link', 
-            { 
-                rel: 'icon', 
-                type: 'image', 
-                href: 'https://www.takurinton.com/me.jpeg' 
-            }
-        ]
+        ['link', { 
+            rel: 'icon', 
+            type: 'image', 
+            href: 'https://www.takurinton.com/me.jpeg' 
+        }], 
+        ['meta',{ name:"keywords", content:"takurinton 技術メモ"}],
+        ['meta',{ name:"og:title", content:"たくりんとん"}],
+        ['meta',{ name:"og:description", content:"たくりんとんの技術メモ"}],
+        ['meta',{ name:"og:type", content:"website"}],
+        ['meta',{ name:"og:url", content:"https://dev.takurinton.com"}],
+        ['meta',{ name:"og:image", content:"https://www.takurinton.com/me.jpeg"}],
+        ['meta',{ name:"twitter:card", content:"summary_large_image"}],
+        ['meta',{ name:"twitter:title", content:"たくりんとん"}],
+        ['meta',{ name:"twitter:description", content:"たくりんとんの技術メモ"}],
+        ['meta',{ name:"twitter:type", content:"website"}],
+        ['meta',{ name:"twitter:url", content:"https://dev.takurinton.com"}],
+        ['meta',{ name:"twitter:image", content:"https://www.takurinton.com/me.jpeg"}],
     ],
     themeConfig: {
       repo: 'takurinton/tech-memo',
       docsBranch: 'main',
-  
       algolia: {
         apiKey: 'b573aa848fd57fb47d693b531297403c',
         indexName: 'vitejs'
--- tech/frontend/index.md
+++ tech/frontend/index.md
@@ -1,3 +1,13 @@
+---
+title: フロントエンドのメモ | たくりんとん
+description: フロントエンドのメモ
+meta:
+ -
+  og:title: 'フロントエンドのメモ'
+ -
+  twitter:title: 'フロントエンドのメモ'
+---
+
 # フロントエンドの技術について
 
 - [json-ld について](/tech/frontend/json-ld)
